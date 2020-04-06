d2153d93816432accaa2f33953347850c298e1b9
Heegon <hgmoon68@gmail.com>
2020-04-06 17:30:01 +0900

    add allinone

--- AllinOne.py
+++ AllinOne.py
@@ -4,6 +4,7 @@ from matplotlib import pyplot as plt
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import random
+from cycler import cycler
 
 
 #from matplotlib import font_manager, rc
@@ -22,7 +23,14 @@ bsObject = BeautifulSoup(html, "html.parser")
 table = bsObject.find(class_= "js-csv-data csv-data js-file-line-container")
 rows = table.select('tbody > tr')
 header = table.select('th')
+
 global ddata 
+global ccount 
+ccount = 0
+
+new_colors = ['b', '#ff7f0e', '#2ca02c', '#d62728',  #'#1f77b4'
+              '#9467bd', '#8c564b', '#e377c2', '#dd1c77',
+              '#31a354', '#17becf', '#fdae6b', '#756bb1']
 
 def flogistic(a,b,c,t): # The logistic function
     return a/(1+np.exp( (b-c*t)/exp_scale) )
@@ -37,7 +45,7 @@ def f(x):   # cost function
 
 def pltdata2(country,region):
     global ddata 
-    ccmap = plt.cm.get_cmap('hsv',20)
+    global ccount 
 
     for row in rows:
         cells = row.select('td')
@@ -51,8 +59,9 @@ def pltdata2(country,region):
                     inflex=[]
                     infley=[]
                     #cclr = np.random.rand(3,)
-                    cclr = ccmap(random.randrange(0,20))
-                    
+                    #cclr = ccmap(random.randrange(0,20))
+                    cclr = new_colors[ccount]
+                    ccount += 1
                     for j in range (0,reg_days):
                         
                         result = optimize.minimize(f, [10, 10,1], method="CG")    
@@ -60,15 +69,21 @@ def pltdata2(country,region):
                         inflex.append( result.x[1]/result.x[2])
                         infley.append(flogistic(result.x[0],result.x[1], result.x[2], inflex[-1]))
 
+                        if j==0: # data and projection plot
+                            edata = [flogistic(result.x[0],result.x[1], result.x[2], t)  for t in range(0,50)]
+                            plt.plot(range(0,len(ddata)), ddata, color=cclr, linewidth = 0.7, label='dat_'+country2+"(cur:"+str(int(ddata[-1]))+")")
+                            plt.plot(range(0,50), edata, color=cclr, linestyle=':',label='prj_'+country2+"(est:"+str(int(result.x[0]))+")")
+
                         del ddata[-1]
 
-                    plt.plot(inflex, infley, color=cclr, marker='.',linestyle=':')
+                    plt.plot(inflex, infley, color=cclr, marker='.',linestyle='--')
                    # plt.plot(inflex[0], infley[0], '>')
-                    plt.plot(inflex[-1], infley[-1], color=cclr,marker="$S$")
+                    plt.plot(inflex[-1], infley[-1], color=cclr,marker='>')
                     plt.plot(inflex[0], infley[0], marker='o',color=cclr)
                     plt.text(inflex[0]+.1, infley[0], country2+'('+str(nsample)+','+str(int(inflex[0]-nsample))+')')
 
                     return 
+
 
 pltdata2("Korea, South","")
 pltdata2("US","")
@@ -89,11 +104,11 @@ day3max = 60-int(np.log10(min_case))*10
 day3line = [min_case*2**((i/3)) for i in range(0,day3max)]
 plt.plot(range(0,day3max), day3line, label='Dbl every 3d',color='k')
 '''
-plt.text(30,3000, 'numbers: (sampling size , days to the inflextion point)')
+plt.text(30,3000, 'numbers: (sampling size , days to the inflextion point)\nPython code: https://github.com/Heegon/PyTest')
 
 
 lastday = str(header[-1].text)
-plt.title('Inflextion point 7-Day Track\nLogistic Regression: corona-19 confirmed cases - the day of '+str(min_case)+'th case to ' +lastday +  '\ndata: '+url + '\nfacebook.com/Heegon.Moon')
+plt.title('Logistic Regression: corona-19 confirmed cases - the day of '+str(min_case)+'th case to ' +lastday +  '\nand Inflextion point 7-Day Track \ndata: '+url + '\nfacebook.com/Heegon.Moon')
 
 plt.yscale('log')
 plt.legend()
